bbab0697e7c8a8d01412a1d4ac33a4e1b2e021d7
Michael Green <mcg216@lehigh.edu>
2015-09-05 08:21:44 -0400

    added jquery file

--- SmartExtension/js/popup.js
+++ SmartExtension/js/popup.js
@@ -1,21 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('educateButton');
-  checkPageButton.addEventListener('click', function() {
+  var checkPageButton = document.getElementsByClassName('btn btn-primary');
+  checkPageButton[0].addEventListener('click', function() {
 
     chrome.tabs.getSelected(null, function(tab) {
       d = document;
 
+
       var f = d.createElement('form');
       //insert backend page here
-      //f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
+      //f.action = 'http://getwreckt.com/home/insult/264';
+      var output = d.getElementsByClassName('output');
+      output[0].innerText = 'You dumb fat shit. No really you are a dumb piece of shit.';
+      //f.method = 'post';
+      //var i = d.createElement('input');
+      //i.type = 'hidden';
+      //i.name = 'url';
+      //i.value = "you dumb fat shit.";
+      //f.appendChild(i);
+      //d.body.appendChild(f);
+      //var h = d.createElement("H1");
+      //var t = d.createTextNode("You dumb fat shit");
+      //h.appendChild(t);
+      //f.submit();
+      //d.body.appendChild(h);
+
     });
   }, false);
 }, false);
+
+$("#button-wrapper").click(function(){
+        alert("Text: " + $("#btn btn-primary").text());
+    });
+
